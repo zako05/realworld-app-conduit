764ff7dd517f70bb6b414b3090ef22135b691b6d
Michal Zak <zako05@gmail.com>
2025-07-02 15:18:03 +0200

    add dotenv to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,13 @@ import express from 'express'
 import http from 'http'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import process from 'process'
+import * as dotenv from 'dotenv'
 import { ApolloServer } from '@apollo/server'
 import { expressMiddleware } from '@apollo/server/express4'
 import { gql } from 'graphql-tag'
+
+dotenv.config({ path: process.env.ENV_FILE ?? '.env' })
 
 const typeDefs = gql`
   type Query {
@@ -21,15 +25,16 @@ const resolvers = {
 const startServer = async () => {
   const app = express()
   const httpServer = http.createServer(app)
-  const port = 4000
+  const port = process.env.PORT || 4000
 
-  const MONGO_URI = 'mongodb://127.0.0.1:27017/realworld_db'
+  const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/realworld_db'
 
   try {
     await mongoose.connect(MONGO_URI)
     console.log('🔌 Connected to MongoDB')
   } catch (err) {
     console.error('❌ Error connecting to MongoDB:', err)
+    process.exit(1)
   }
 
   const server = new ApolloServer({
